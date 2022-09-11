cb353d3dd444e188e7d4e69a0c1d3f7a16d6e973
Kez Anwar <kezanwar@gmail.com>
2022-09-11 14:45:43 +0100

    changed category svgs to pngs

--- utilities/media.js
+++ utilities/media.js
@@ -1,6 +1,6 @@
 module.exports.PRODUCT_CATEGORY_SVGS = {
   confectionary:
-    'https://www.knowledgebase.therealomg.com/wp-content/uploads/2022/09/confectionary.svg',
+    'https://www.knowledgebase.therealomg.com/wp-content/uploads/2022/09/confectionary.png',
   dried_fruit:
-    'https://www.knowledgebase.therealomg.com/wp-content/uploads/2022/09/dried-fruit.svg',
+    'https://www.knowledgebase.therealomg.com/wp-content/uploads/2022/09/dried-fruit.png',
 }
